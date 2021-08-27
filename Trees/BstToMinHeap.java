0ae39eff86e38c8eb0614fb277f8806e49cb1a6d
AnushriDubey <89227101+AnushriDubey@users.noreply.github.com>
2021-08-28 01:35:50 +0530

    Create BstToMinHeap.java

---
+++ Trees/BstToMinHeap.java
@@ -0,0 +1,13 @@
+/*
+solve it later and add the code.
+
+Adding description - 
+Steps to follow of this:
+
+1. Do the inorder traversal of BST (it will give you ascending order sorted array.)
+2. Now run preoredr traversal on this BST and start copying values from inorder traversal.
+
+
+
+*/
+
